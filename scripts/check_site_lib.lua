edc680de230b0d210cdbda13a9269cd8ee35ce8a
lemoer <git@irrelefant.net>
2017-10-28 17:05:53 +0200

    scripts: check_site_lib: introduce need_domain_* and need_site_*

--- scripts/check_site_lib.lua
+++ scripts/check_site_lib.lua
@@ -1,9 +1,32 @@
 local function loadvar(varname)
-   local ok, val = pcall(assert(loadstring('return site.' .. varname)))
-   if ok then
-      return val
+   local ok, val = pcall(assert(loadstring('return domain.' .. varname)))
+   if ok and val ~= nil then
+      return val, 'domains/'..domain_code..'.conf'
+   end
+
+   ok, val = pcall(assert(loadstring('return site.' .. varname)))
+   if ok and val ~= nil then
+      return val, 'site.conf'
    else
-      return nil
+      return nil, 'site.conf or domains/'..domain_code..'.conf'
+   end
+end
+
+local function loadvar_domain(varname)
+   local ok, val = pcall(assert(loadstring('return domain.' .. varname)))
+   if ok then
+      return val, 'domains/'..domain_code..'.conf'
+   else
+      return nil, 'domains/'..domain_code..'.conf'
+   end
+end
+
+local function loadvar_site(varname)
+   ok, val = pcall(assert(loadstring('return site.' .. varname)))
+   if ok then
+      return val, 'site.conf'
+   else
+      return nil, 'site.conf'
    end
 end
 
@@ -33,67 +56,75 @@ local function assert_type(var, t, msg)
 end
 
 
-function assert_uci_name(var)
+function forbid_in_domain(varname)
+   local ok, val = pcall(assert(loadstring('return domain.' .. varname)))
+   assert(not ok or val == nil, "domains/"..domain_code..".conf error: `"..varname.."` is not allowed in domain specific config.")
+end
+
+function forbid_in_site(varname)
+   local ok, val = pcall(assert(loadstring('return site.' .. varname)))
+   assert(not ok or val == nil, "site.conf error: `"..varname.."` is not allowed in site config.")
+end
+
+function assert_uci_name(var, conf_name)
    -- We don't use character classes like %w here to be independent of the locale
-   assert(var:match('^[0-9a-zA-Z_]+$'), "site.conf error: `" .. var .. "' is not a valid config section name (only alphanumeric characters and the underscore are allowed)")
+   assert(var:match('^[0-9a-zA-Z_]+$'), conf_name.." error: `" .. var .. "' is not a valid config section name (only alphanumeric characters and the underscore are allowed)")
 end
 
 
-function need_string(varname, required)
-   local var = loadvar(varname)
+function _need_string(loadvar, varname, required)
+   local var, conf_name = loadvar(varname)
 
    if required == false and var == nil then
-      return nil
+      return nil, conf_name
    end
 
-   assert_type(var, 'string', "site.conf error: expected `" .. varname .. "' to be a string")
-   return var
+   assert_type(var, 'string', conf_name .. " error: expected `" .. varname .. "' to be a string")
+   return var, conf_name
 end
 
-function need_string_match(varname, pat, required)
-   local var = need_string(varname, required)
+function _need_string_match(loadvar, varname, pat, required)
+   local var, conf_name = _need_string(loadvar, varname, required)
 
    if not var then
       return nil
    end
 
-   assert(var:match(pat), "site.conf error: expected `" .. varname .. "' to match pattern `" .. pat .. "'")
+   assert(var:match(pat), conf_name.." error: expected `" .. varname .. "' to match pattern `" .. pat .. "'")
 
    return var
 end
 
-function need_number(varname, required)
-   local var = loadvar(varname)
+function _need_number(loadvar, varname, required)
+   local var, conf_name = loadvar(varname)
 
    if required == false and var == nil then
       return nil
    end
 
-   assert_type(var, 'number', "site.conf error: expected `" .. varname .. "' to be a number")
+   assert_type(var, 'number', conf_name.." error: expected `" .. varname .. "' to be a number")
 
    return var
 end
 
-function need_boolean(varname, required)
-   local var = loadvar(varname)
+function _need_boolean(loadvar, varname, required)
+   local var, conf_name = loadvar(varname)
 
    if required == false and var == nil then
       return nil
    end
 
-   assert_type(var, 'boolean', "site.conf error: expected `" .. varname .. "' to be a boolean")
+   assert_type(var, 'boolean', conf_name.." error: expected `" .. varname .. "' to be a boolean")
 
    return var
 end
 
-function need_array(varname, subcheck, required)
-   local var = loadvar(varname)
-
+local function __need_array_from_var(var, varname, subcheck, required, conf_name)
    if required == false and var == nil then
       return nil
    end
 
-   assert_type(var, 'table', "site.conf error: expected `" .. varname .. "' to be an array")
+   assert_type(var, 'table', conf_name.." error: expected `" .. varname .. "' to be an array")
 
    for _, e in ipairs(var) do
       subcheck(e)
@@ -102,50 +133,89 @@ function need_array(varname, subcheck, required)
    return var
 end
 
-function need_table(varname, subcheck, required)
-   local var = loadvar(varname)
+function _need_array(loadvar, varname, subcheck, required)
+   local var, conf_name = loadvar(varname)
+   return __need_array_from_var(var, varname, subcheck, required, conf_name)
+end
+
+
+function _need_table(loadvar, varname, subcheck, required)
+   local var, conf_name = loadvar(varname)
 
    if required == false and var == nil then
       return nil
    end
 
-   assert_type(var, 'table', "site.conf error: expected `" .. varname .. "' to be a table")
+   assert_type(var, 'table', conf_name.." error: expected `" .. varname .. "' to be a table")
 
    if subcheck then
       for k, v in pairs(var) do
-         subcheck(k, v)
+         subcheck(k, v, conf_name)
       end
    end
 
    return var
 end
 
-function need_one_of(varname, array, required)
-   local var = loadvar(varname)
+function _need_one_of(loadvar, varname, array, required)
+   local var, conf_name = loadvar(varname)
 
    if required == false and var == nil then
       return nil
    end
 
-   assert_one_of(var, array, "site.conf error: expected `" .. varname .. "' to be one of given array: " .. array_to_string(array))
+   assert_one_of(var, array, conf_name.." error: expected `" .. varname .. "' to be one of given array: " .. array_to_string(array))
 
    return var
 end
 
-function need_string_array(varname, required)
-   local ok, var = pcall(need_array, varname, function(e) assert_type(e, 'string') end, required)
-   assert(ok, "site.conf error: expected `" .. varname .. "' to be a string array")
+function _need_string_array(loadvar, varname, required)
+   local var, conf_name = loadvar(varname)
+   local ok, var = pcall(__need_array_from_var, var, varname, function(e) assert_type(e, 'string') end, required, conf_name)
+   assert(ok, conf_name.." error: expected `" .. varname .. "' to be a string array")
    return var
 end
 
-function need_string_array_match(varname, pat, required)
-   local ok, var = pcall(need_array, varname, function(e) assert(e:match(pat)) end, required)
-   assert(ok, "site.conf error: expected `" .. varname .. "' to be a string array matching pattern `" .. pat .. "'")
+function _need_string_array_match(loadvar, varname, pat, required)
+   local var, conf_name = loadvar(varname)
+   local ok, var = pcall(__need_array_from_var, var, varname, function(e) assert(e:match(pat)) end, required, conf_name)
+   assert(ok, conf_name.." error: expected `" .. varname .. "' to be a string array matching pattern `" .. pat .. "'")
    return var
 end
 
-function need_array_of(varname, array, required)
-   local ok, var = pcall(need_array, varname, function(e) assert_one_of(e, array) end,required)
-   assert(ok, "site.conf error: expected `" .. varname .. "' to be a subset of given array: " .. array_to_string(array))
+function _need_array_of(loadvar, varname, array, required)
+   local var, conf_name = loadvar(varname)
+   local ok, var = pcall(__need_array_from_var, var, varname, function(e) assert_one_of(e, array) end, required, conf_name)
+   assert(ok, conf_name.." error: expected `" .. varname .. "' to be a subset of given array: " .. array_to_string(array))
    return var
+end
+
+-- wrap need_* in need_site_* and need_domain_*
+
+local function wrap_need(fun, with, loadvar_fun)
+   return function(varname, ...)
+      if with ~= nil then
+         with(varname)
+      end
+      return fun(loadvar_fun, varname, unpack({...}))
+   end
+end
+
+local need_names = {
+   'string',
+   'string_match',
+   'number',
+   'boolean',
+   'array',
+   'table',
+   'one_of',
+   'string_array',
+   'string_array_match',
+   'array_of'
+}
+for _, k in pairs(need_names) do
+   local fun = _G['_need_'..k]
+   _G['need_'..k] = wrap_need(fun, nil, loadvar)
+   _G['need_site_'..k] = wrap_need(fun, forbid_in_domain, loadvar_site)
+   _G['need_domain_'..k] = wrap_need(fun, forbid_in_site, loadvar_domain)
 end
